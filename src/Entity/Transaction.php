d8048d6e1ffc372dc2281d7ee82a1c9762e05659
anaxcii <85498508+anaxcii@users.noreply.github.com>
2023-10-03 18:43:37 +0200

    Entity BDD

---
+++ src/Entity/Transaction.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\TransactionRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TransactionRepository::class)]
+#[ApiResource]
+class Transaction
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?float $price_buy = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $transaction_date = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPriceBuy(): ?float
+    {
+        return $this->price_buy;
+    }
+
+    public function setPriceBuy(float $price_buy): static
+    {
+        $this->price_buy = $price_buy;
+
+        return $this;
+    }
+
+    public function getTransactionDate(): ?\DateTimeInterface
+    {
+        return $this->transaction_date;
+    }
+
+    public function setTransactionDate(\DateTimeInterface $transaction_date): static
+    {
+        $this->transaction_date = $transaction_date;
+
+        return $this;
+    }
+}
