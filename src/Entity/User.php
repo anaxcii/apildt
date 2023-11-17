ef36f508171aec88c91b1b3a65784e698ae3dc44
anaxcii <85498508+anaxcii@users.noreply.github.com>
2023-11-17 17:39:07 +0100

    price

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -295,7 +295,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     public function setMoney(float $money): static
     {
-        $this->money = $money = '100';
+        $this->money = 100;
 
         return $this;
     }
