c509c20cc094a0771f8f5f667cdda85ff06066e3
anaxcii <85498508+anaxcii@users.noreply.github.com>
2023-11-19 14:10:33 +0100

    owner modif et patch nft

--- src/Entity/Nft.php
+++ src/Entity/Nft.php
@@ -31,8 +31,8 @@ use Symfony\Component\Serializer\Annotation\Groups;
     new Get(),
     new GetCollection(),
     new Post(security: "is_granted('IS_AUTHENTICATED_FULLY')"),
-    new Patch(security: "is_granted('ROLE_ADMIN') or object.nftgallery.creator == user"),
-    new Delete(security: "is_granted('ROLE_ADMIN') or object.nftgallery.creator == user"),
+    new Patch(security: "is_granted('ROLE_ADMIN') or object.owner == user"),
+    new Delete(security: "is_granted('ROLE_ADMIN') or object.owner == user"),
 
     new Post(uriTemplate: 'api/nft/{id}/sell', routeName: 'app_nft_sell', name: 'app_nft_sell',),
     new Get(uriTemplate: 'api/nft/{id}/buy', routeName: 'app_nft_buy', name: 'app_nft_buy',),
